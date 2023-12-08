c796d687d125d0de0ed0844440b3f3ad6186f011
green_sjkim <green2318@o2o.kr>
2023-12-08 18:44:29 +0900

    Feat: Login API 구현

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -13,3 +13,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):  # validated_data : the data will have benn validated.
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=128, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'token']
+
+        read_only_fields = ['token']  # token is going to be served by application
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
-from authentication.serializers import RegisterSerializer
+from authentication.serializers import RegisterSerializer, LoginSerializer
 
 
 class RegisterAPIVIew(GenericAPIView):
@@ -16,3 +17,22 @@ class RegisterAPIVIew(GenericAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        # when a user doesn't send these keys, gate will throw a key error
+        # so first, what you want to do now is do 'None' here
+        email = request.data.get('email', None)  # <-- None
+        password = request.data.get('password', None)
+
+        user = authenticate(username=email, password=password)
+
+        if user:  # if user, then log them in. and send them token.
+            serializer = self.serializer_class(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response({'message': 'invalid credentials. try again'},  # authentication is failed
+                        status=status.HTTP_401_UNAUTHORIZED)
