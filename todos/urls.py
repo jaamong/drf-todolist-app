cae7e37f5184b480e7beb60c4b1a5faf0947eb46
green_sjkim <green2318@o2o.kr>
2023-12-11 15:38:51 +0900

    Feat: Todo detail url 구현

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ListCreateTodosAPIView
+from .views import ListCreateTodosAPIView, TodoDetailAPIView
 
 urlpatterns = [
     path('', ListCreateTodosAPIView.as_view(), name='create-list-todo'),
     # path('create', CreateTodoAPIView.as_view(), name='create-todo'),
     # path('list', ListTodoAPIView.as_view(), name='list-todo'),
+    path('<int:id>', TodoDetailAPIView.as_view(), name='detail-todo'),
 ]
