24cdeab71ef2cd4665e218177a60c31c6644d6b0
green_sjkim <green2318@o2o.kr>
2023-12-11 15:02:10 +0900

    Feat: create, list 공통 url

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CreateTodoAPIView, ListTodoAPIView
+from .views import ListCreateTodosAPIView
 
 urlpatterns = [
-    path('create', CreateTodoAPIView.as_view(), name='create-todo'),
-    path('list', ListTodoAPIView.as_view(), name='list-todo'),
+    path('', ListCreateTodosAPIView.as_view(), name='create-list-todo'),
+    # path('create', CreateTodoAPIView.as_view(), name='create-todo'),
+    # path('list', ListTodoAPIView.as_view(), name='list-todo'),
 ]
