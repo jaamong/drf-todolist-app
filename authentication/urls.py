f7bb65521aff6f6d8a423ffd3c038af063e5fb92
green_sjkim <green2318@o2o.kr>
2023-12-11 12:07:24 +0900

    Feat: AuthUser url 추가

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,5 @@ from authentication import views
 urlpatterns = [
     path('register', views.RegisterAPIVIew.as_view(), name='register'),
     path('login', views.LoginAPIView.as_view(), name='login'),
+    path('user', views.AuthUserAPIView.as_view(), name='user'),
 ]
