e92a63c47e61801acaf8b3608c3604135888e389
jaamong <amypure225@naver.com>
2023-12-24 16:01:57 +0900

    Feat: filtering 기능 구현
    django_filter 사용

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,6 +12,11 @@ class ListCreateTodosAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = [IsAuthenticated, ]
     authentication_classes = [authentication.jwt.JWTAuthentication]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['id', 'title', 'desc', 'is_complete']
+    search_fields = ['id', 'title', 'desc', 'is_complete']
+    ordering_fields = ['id', 'title', 'desc', 'is_complete']
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
